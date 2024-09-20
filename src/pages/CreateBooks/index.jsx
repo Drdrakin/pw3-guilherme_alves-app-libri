1f4fb1f311dc680b0a06dd21f76c30985b53e0fa
Guilherme Alves <drdrakino@gmail.com>
2024-09-19 22:24:55 -0300

    feat: Primeiro Fetch()

--- src/pages/CreateBooks/index.jsx
+++ src/pages/CreateBooks/index.jsx
@@ -3,8 +3,34 @@ import styles from './createBooks.module.css'
 import Input from '../../components/Forms/Input';
 import Select from '../../components/Forms/Select';
 import Button from '../../components/Forms/Button';
+import { useState, useEffect } from 'react';
 
 const CreateBooks = () => {
+
+  useEffect(() => {
+    fetch('http://localhost:5000/listagemCateorias', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*'
+      }
+    })
+    .then(
+      (res) => res.json()
+    )
+    .then(
+      (data) => {
+        console.log('data: ' + data.data[0].nome_categoria)
+      }
+    )
+    .catch(
+      (error) =>{
+        console.log(error)
+      }
+    )
+  }, []);
+
   return (
     <section className={styles.createBookContainer}>
         <h1>Cadastrar novos livros</h1>
